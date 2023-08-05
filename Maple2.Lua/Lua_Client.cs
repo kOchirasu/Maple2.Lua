6c088ea135fdb392fd56ab216e17fa8a28fbe774
Zin <62830952+Zintixx@users.noreply.github.com>
2023-08-05 09:43:58 -0700

    Define CalcItemLevel params

--- Maple2.Lua/Lua_Client.cs
+++ Maple2.Lua/Lua_Client.cs
@@ -281,17 +281,14 @@ public partial class Lua {
     [GlobalFunction(Name = "calcGatheringActionCount")]
     public partial long CalcGatheringActionCount(float a0, int a1, int a2, int a3, int a4);
 
-    // a2 = option_diff[3] related...
     [GlobalFunction(Name = "calcEnchantExpGainExp")]
-    public partial int CalcEnchantExpGainExp(ushort levelLimit, int grade, int a2, int type);
+    public partial int CalcEnchantExpGainExp(ushort levelLimit, int grade, int enchantLevel, int type);
 
-    // a2 = option_diff[3] related...
     [GlobalFunction(Name = "calcEnchantExpGainExpNew")]
-    public partial int CalcEnchantExpGainExpNew(ushort levelLimit, int grade, int a2, int type);
+    public partial int CalcEnchantExpGainExpNew(ushort levelLimit, int grade, int enchantLevel, int type);
 
-    // a3 = option_diff[3] related...
     [GlobalFunction(Name = "calcItemLevel")]
-    public partial (int, int) CalcItemLevel(float gearScore, int grade, int type, int a3, int levelLimit);
+    public partial (int, int) CalcItemLevel(float gearScore, int grade, int type, int enchantLevel, int limitBreakLevel);
 
     [GlobalFunction(Name = "calcOHItemAdditionalLevel")]
     public partial (int, int) CalcOHItemAdditionalLevel(int a0, int a1, int a2, int a3);
